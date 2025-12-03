0b9aaa89ab73c004a6b0c8b50ac5b9ac89508d1e
Tihara <tiharaeg@gmail.com>
2025-12-03 14:59:34 +0530

    Initial Commit

---
+++ iTour Tutorial/iTour_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTour_TutorialApp.swift
+//  iTour Tutorial
+//
+//  Created by Tihara Egodage on 2025-12-03.
+//
+
+import SwiftUI
+
+@main
+struct iTour_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
